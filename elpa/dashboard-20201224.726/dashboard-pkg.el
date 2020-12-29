073c788fdb7ea6294439e06415faf1ea9f50569d
wutchy <dp4dp7@outlook.jp>
2020-12-29 13:18:10 +0900

    パッケージ関連のファイルもgit管理対象にした。

---
+++ elpa/dashboard-20201224.726/dashboard-pkg.el
@@ -0,0 +1,13 @@
+(define-package "dashboard" "20201224.726" "A startup screen extracted from Spacemacs"
+  '((emacs "25.3")
+    (page-break-lines "0.11"))
+  :commit "ef51ad205c6f21e80cb1e4b2ab2d0547675ea206" :authors
+  (("Rakan Al-Hneiti"))
+  :maintainer
+  ("Rakan Al-Hneiti")
+  :keywords
+  ("startup" "screen" "tools" "dashboard")
+  :url "https://github.com/emacs-dashboard/emacs-dashboard")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
